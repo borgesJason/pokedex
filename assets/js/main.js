c2ad52657d2c243a60ff24873222e2a7eeb2ba65
borgesJason <jgbs6@outlook.com>
2023-10-03 04:43:18 -0300

    Primeiro commit projeto Pokedex integrado com PokeApi

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonHtml(pokemon){
+    return `            
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}" 
+            alt="${pokemon.name}">
+        </div>
+    </li>
+`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonHtml).join('')
+        pokemonList.innerHTML += newHtml
+})
